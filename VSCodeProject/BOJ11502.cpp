18208799e9a646be2b505514b59e39f9a629dcda
iJoom <ghfdld@naver.com>
2021-05-26 14:08:03 +0900

    solved boj 11502 에라토스테네스의 체 + 단순 BF input 값이 작으므로

---
+++ VSCodeProject/BOJ11502.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int m[1000]; // 에라토스테네스의 체
+
+void eratos() {
+   for(int i=2;i<1000;i++) {
+       if(m[i]==1)continue;
+       for(int k=i+i;k<1000;k+=i){
+           m[k]=1;
+       }
+   }
+}
+
+int main() {
+    int cnt;
+    int judge = 0;
+
+    eratos(); // m[] == 0 이면 소수 , 1이면 소수X
+    cin>>cnt;
+    int num;
+    while(cnt>0){
+        cnt--;
+
+        judge = 0;
+        cin>>num;
+
+        for(int a=2;a<1000;a++) {
+            if(m[a]==1)continue;
+        for(int b=2;b<1000;b++){
+                if(m[b]==1)continue;
+            for(int c=2;c<1000;c++) {
+                 if(m[c]==1)continue;
+                if((a+b+c) == num) {
+                    cout<<a<<' '<<b<<' '<<c<<endl;
+                    judge = 1;
+                    break;
+                }
+            }
+            if(judge == 1)break;
+        }
+        if(judge == 1)break;
+    }
+    if(judge != 1)cout<<0<<endl;
+
+    }
+    
+
+    return 0;
+}
