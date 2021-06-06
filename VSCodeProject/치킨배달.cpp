b1c7ef54aee6349e516b34cdce3c611d67c5ab1b
iJoom <ghfdld@naver.com>
2021-06-06 19:01:26 +0900

    add remark

--- VSCodeProject/치킨배달.cpp
+++ VSCodeProject/치킨배달.cpp
@@ -23,22 +23,26 @@ int main(){
         }
     }
 
-    vector<int> v(chicken.size(),1); // 치킨 m 개 뽑는 순열 조건 만들기
+    vector<int> v(chicken.size(),1); // 치킨 m 개 뽑는 조합 조건 만들기
     fill(v.begin(),v.begin()+chicken.size()-m,0);
+    //5개중 3개 조합 , 00111 , next_permutation은 오름차순 정렬
      int dist =0;
      int answer = INT_MAX;
     do
     {
-   
+        dist = 0;
+
         for(int i=0;i<home.size();i++) {
             int chicdist =15000;
             for(int j=0;j<chicken.size();j++){
            if(v[j]==0)continue;
+            cout<<chicken[j].first<<chicken[j].second<<endl;
             chicdist = min(abs(home[i].first-chicken[j].first)+abs(home[i].second-chicken[j].second),chicdist);
         }
+        cout<<"껄껄";
         dist += chicdist;
         }
-        answer = min(dist,dist);
+        answer = min(answer,dist);
     
     } while (next_permutation(v.begin(),v.end()));
     
