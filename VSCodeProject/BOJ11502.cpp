8367f02a7ce5d8ca3e669915190e7698a978a719
iJoom <ghfdld@naver.com>
2021-06-11 13:21:52 +0900

    edit remark

--- VSCodeProject/BOJ11502.cpp
+++ VSCodeProject/BOJ11502.cpp
@@ -22,50 +22,43 @@ void eratos() {
 int main() {
     int cnt;
     int judge = 0;
-    clock_t start,end;
-    
-    
 
-    start = clock();
     eratos(); // m[] == 0 이면 소수 , 1이면 소수X
-    end = clock();
-    double duration = (double)(end-start)/CLOCKS_PER_SEC;
-    cout<<duration<<endl;
 
-    // int ck=0;
-    // for(int i=2;i<1000;i++){
-    //     if(m[i]==0)cout<<i<<' ';
-    //     ck++;
-    //     if(ck%10==0)cout<<endl;
-    // }
+    int ck=0;
+    for(int i=2;i<1000;i++){
+        if(m[i]==0)cout<<i<<' ';
+        ck++;
+        if(ck%10==0)cout<<endl;
+    }
     
-    
-    // cin>>c
-    // while(cnt>0){
-    //     cnt--;
+    int num;
+    cin>>cnt;
+    while(cnt>0){
+        cnt--;
 
-    //     judge = 0;
-    //     cin>>num;
+        judge = 0;
+        cin>>num;
 
-    //     for(int a=2;a<1000;a++) {
-    //         if(m[a]==1)continue;
-    //     for(int b=2;b<1000;b++){
-    //             if(m[b]==1)continue;
-    //         for(int c=2;c<1000;c++) {
-    //              if(m[c]==1)continue;
-    //             if((a+b+c) == num) {
-    //                 cout<<a<<' '<<b<<' '<<c<<endl;
-    //                 judge = 1;
-    //                 break;
-    //             }
-    //         }
-    //         if(judge == 1)break;
-    //     }
-    //     if(judge == 1)break;
-    // }
-    // if(judge != 1)cout<<0<<endl;
+        for(int a=2;a<1000;a++) {
+            if(m[a]==1)continue;
+        for(int b=2;b<1000;b++){
+                if(m[b]==1)continue;
+            for(int c=2;c<1000;c++) {
+                 if(m[c]==1)continue;
+                if((a+b+c) == num) {
+                    cout<<a<<' '<<b<<' '<<c<<endl;
+                    judge = 1;
+                    break;
+                }
+            }
+            if(judge == 1)break;
+        }
+        if(judge == 1)break;
+    }
+    if(judge != 1)cout<<0<<endl;
 
-    // }
+    }
     
 
     return 0;
